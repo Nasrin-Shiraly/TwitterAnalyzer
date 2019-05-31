a38bca72ac59523df928edfab1ca93d7d59c1f32
Nasrin <N.shiraly@gmail.com>
2019-05-31 17:36:18 +0200

    keyword_analysis updated. test included. test data, i.e. tweets.json updated

--- data_set_preparation/keyword_analysis.py
+++ data_set_preparation/keyword_analysis.py
@@ -1,4 +1,7 @@
 import json
+from collections import Counter
+import pandas as pd
+from past.builtins import xrange
 
 from db_collection_structures.interaction import Interaction
 from utils.utilities import formatted_created_at
@@ -28,16 +31,23 @@ class KeywordAnalysis:
         query_ids = list(cursor.aggregate(pipeline_ids))
         query_names = list(cursor.aggregate(pipeline_names))
         number_of_tweets = list(cursor.aggregate(pipeline_number_of_tweets))
+
         len_unique_ids = len(set([twitter_id['_id'] for twitter_id in query_ids]))
+
         user_ids_repeated_more_than_threshold = [{'id': each['_id'], 'count': each['count']} for each in query_ids if
                                                  each['count'] > threshold]
+
         user_names_repeated_more_than_threshold = [{'name': each['_id'], 'count': each['count']} for each in query_names
                                                    if each['count'] > threshold]
 
-        id_and_names_with_repetitions = [{'id': user_id['id'], 'screen_name': name['name'], 'count': user_id['count']}
-                                         for pos, user_id in enumerate(user_ids_repeated_more_than_threshold) for
-                                         position, name in enumerate(user_names_repeated_more_than_threshold) if
-                                         pos == position]
+        length = len(user_names_repeated_more_than_threshold)
+
+        id_and_names_with_repetitions = [{'id': user_ids_repeated_more_than_threshold[pos]['id'],
+                                          'screen_name': user_names_repeated_more_than_threshold[pos]['name'],
+                                          'count': user_ids_repeated_more_than_threshold[pos]['count']}
+                                         for pos in xrange(0, length)]
+
+        print(id_and_names_with_repetitions)
 
         with open(self.pwd / 'most_seen_users.json', 'w') as report:
             json.dump(id_and_names_with_repetitions, report)
@@ -57,7 +67,6 @@ class KeywordAnalysis:
 
         interaction = Interaction(collection=collection, db=self.db_name, db_url=self.db_url)
         for pos, item in enumerate(cursor):
-            print(f"tweet number {pos}")
             account = item["tweet_object"]['user']['screen_name']
             created_at = formatted_created_at(item["tweet_object"]['created_at'])
 
@@ -80,3 +89,50 @@ class KeywordAnalysis:
                                                     interaction_towards=account, interaction_type='tweet')
         if cursor:
             del cursor
+
+    def redundant_tweet_analysis(self, excluded_list):
+        req_fields = {'tweet_object.user.screen_name': 1,
+                      "tweet_object.retweeted_status.user.screen_name": 1,
+                      "tweet_object.text": 1, 'tweet_object.timestamp_ms': 1,
+                      }
+        cursor = self.db[self.collection].find({}, req_fields)
+        main_cursor = self.db[self.collection]
+        columns = ['account', 'copied_tweet', 'copy_from_account']
+        copied_tweets = pd.DataFrame(columns=columns)
+        index = 0
+        for pos, item in enumerate(cursor):
+            print(f"analyzing redundant tweets for the tweet number: {pos}")
+            account = item["tweet_object"]['user']['screen_name']
+            text = item["tweet_object"]['text']
+            if text.lower() not in excluded_list:
+                try:
+                    _ = item["tweet_object"]["retweeted_status"]['user']['screen_name']
+                except KeyError:
+                    pipeline = [{"$match": {"tweet_object.text": {"$regex": "^(?!RT @)"}}},
+                                {"$match": {"tweet_object.text": {"$eq": text}}},
+                                {"$match": {"tweet_object.timestamp_ms": {
+                                    "$lt": item["tweet_object"]["timestamp_ms"]}}},
+                                {"$sort": {"tweet_object.timestamp_ms": 1}},
+                                {"$limit": 1}]
+
+                    query = list(main_cursor.aggregate(pipeline))
+                    if query:
+                        index += 1
+                        copied_tweets.loc[index, "account"] = account
+                        copied_tweets.loc[index, "copied_tweet"] = text
+                        copied_tweets.loc[index, "copy_from_account"] = str(
+                            query[0]["tweet_object"]["user"]["screen_name"]).replace(r"\n", " ")
+                        print(f"{account} has coppied {copied_tweets.loc[index, 'copy_from_account']}")
+            else:
+                print(f"{text} will not be analyzed as it's in {excluded_list}")
+
+        if cursor:
+            del cursor
+
+        print('done!')
+
+        most_copied_by = sorted(Counter(copied_tweets["account"]), key=lambda x: x[1], reverse=True)
+        most_copied_from = sorted(Counter(copied_tweets["copy_from_account"]), key=lambda x: x[1], reverse=True)
+        with open(str(self.pwd / 'copied_tweets.csv'), 'w') as f:
+            copied_tweets.to_csv(f)
+        return most_copied_by, most_copied_from
---
+++ tests/test_keyword_analysis.py
@@ -0,0 +1,82 @@
+import json
+import shutil
+import tempfile
+from pathlib import Path
+from unittest import TestCase
+from past.builtins import xrange
+
+from data_set_preparation.keyword_analysis import KeywordAnalysis
+from db_collection_structures.tweet import Tweet
+
+import pandas as pd
+
+
+class MockCollection:
+    @staticmethod
+    def mock_tweet_collection(dummy_tweet_pwd, tweet):
+        with open(str(dummy_tweet_pwd), "r") as dummy_tweets:
+            sample_tweets = json.load(dummy_tweets)
+
+        for twt in sample_tweets:
+            tweet.document_insert(twt)
+
+
+class TestKeywordAnalysis(TestCase):
+
+    def setUp(self):
+        self.db_alias = 'dummyDB'
+        collection = 'dummyCollection'
+        db_url = 'localhost:27017'
+        self.tweet_handle = Tweet(collection='dummyCollection', db='dummyDB', db_url='localhost:27017')
+
+        mock_tweets = Path(__file__).parent / 'data' / 'tweets.json'
+        self.pwd = Path(tempfile.mktemp())
+        self.pwd.mkdir(parents=True, exist_ok=True)
+        self.target = KeywordAnalysis(db_alias=self.db_alias, collection=collection,
+                                      artifacts_path=self.pwd, db_url=db_url)
+        mock_test_preparation = MockCollection()
+        mock_test_preparation.mock_tweet_collection(mock_tweets, self.tweet_handle)
+
+    def tearDown(self):
+        self.tweet_handle.conn.db_disconnection()
+        self.tweet_handle.conn.client.drop_database(self.db_alias)
+        shutil.rmtree(self.pwd)
+
+    def test_number_of_users_and_tweets(self):
+        expected_result = [{'number_of_streamed_tweets': 5}]
+        actual_results = self.target.number_of_users_and_tweets(1)
+
+        self.assertEqual(actual_results[0], expected_result)
+        self.assertEqual(actual_results[1], 4)
+        self.assertTrue((self.pwd / "most_seen_users.json").is_file())
+
+    def test_interactions(self):
+        expected_results = [{'interaction_type': 'reply', 'interaction_owner': 'USER_2',
+                             'interaction_towards': 'USER_1'}, {'interaction_type': 'retweet',
+                                                                'interaction_owner': 'USER_1',
+                                                                'interaction_towards': 'USER_3'}, {
+                                'interaction_type': 'tweet', 'interaction_owner': 'USER_3',
+                                'interaction_towards': 'USER_3'}, {'interaction_type': 'tweet',
+                                                                   'interaction_owner': 'USER_2',
+                                                                   'interaction_towards': 'USER_2'}, {
+                                'interaction_type': 'quote', 'interaction_owner': 'USER_4',
+                                'interaction_towards': 'USER_1'}]
+
+        self.target.interactions('interaction_collection')
+        actual_results = [each for each in self.target.db['interaction_collection'].find()]
+
+        for pos in xrange(0, len(expected_results)):
+            self.assertEqual(expected_results[pos]['interaction_type'], actual_results[pos]["interaction_type"])
+            self.assertEqual(expected_results[pos]['interaction_owner'], actual_results[pos]["interaction_owner"])
+            self.assertEqual(expected_results[pos]['interaction_towards'], actual_results[pos]["interaction_towards"])
+
+    def test_redundent_tweets(self):
+        most_copied_by, most_copied_from = self.target.redundant_tweet_analysis([])
+        self.assertEqual(len(most_copied_by), 1)
+        self.assertEqual(most_copied_by, ['USER_2'])
+        self.assertEqual(most_copied_from, ['USER_3'])
+        self.assertTrue((self.pwd / 'copied_tweets.csv').is_file())
+        with open(str(self.pwd / 'copied_tweets.csv'), 'r') as f:
+            copied_tweets = pd.read_csv(f)
+
+        self.assertEqual(copied_tweets.loc[0, "copied_tweet"], "user_3 has tweeted")
